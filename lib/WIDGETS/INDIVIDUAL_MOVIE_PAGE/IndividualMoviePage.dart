e88b982d54d76d996dbc43388d3b5823c6418239
hitesh parmar <hiteshparmar16320@gmail.com>
2023-01-28 21:50:21 +0530

    ADDED IM PAGE

--- lib/WIDGETS/INDIVIDUAL_MOVIE_PAGE/IndividualMoviePage.dart
+++ lib/WIDGETS/INDIVIDUAL_MOVIE_PAGE/IndividualMoviePage.dart
@@ -1,5 +1,6 @@
 import 'package:cruise_blade/WIDGETS/BigTextWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class IndiVidualMoviePage extends StatefulWidget {
   const IndiVidualMoviePage({super.key});
@@ -9,6 +10,52 @@ class IndiVidualMoviePage extends StatefulWidget {
 }
 
 class _IndiVidualMoviePageState extends State<IndiVidualMoviePage> {
+  bool ticked = false;
+  int currIndex = 0;
+  void indexChanger(int index) {
+    setState(() {
+      currIndex = index;
+      print(currIndex);
+    });
+  }
+
+  final List movieDates = [
+    {
+      "Day": "Mon",
+      "Date": "1",
+      "Time": "1:00",
+    },
+    {
+      "Day": "Tues",
+      "Date": "2",
+      "Time": "2:00",
+    },
+    {
+      "Day": "Wed",
+      "Date": "3",
+      "Time": "3:00",
+    },
+    {
+      "Day": "Thu",
+      "Date": "4",
+      "Time": "4:00",
+    },
+    {
+      "Day": "Fri",
+      "Date": "5",
+      "Time": "5:00",
+    },
+    {
+      "Day": "Sat",
+      "Date": "6",
+      "Time": "6:00",
+    },
+    {
+      "Day": "Sun",
+      "Date": "7",
+      "Time": "7:00",
+    },
+  ];
   bool expandedMovieDiscription = false;
   void expansionChanger() {
     setState(() {
@@ -32,7 +79,8 @@ class _IndiVidualMoviePageState extends State<IndiVidualMoviePage> {
         decoration: BoxDecoration(
           image: DecorationImage(
             image: const NetworkImage(
-                "https://wallpaperaccess.com/full/96632.jpg"),
+              "https://wallpaperaccess.com/full/96632.jpg",
+            ),
             fit: MediaQuery.of(context).orientation == Orientation.portrait
                 ? BoxFit.fill
                 : BoxFit.cover,
@@ -106,8 +154,205 @@ class _IndiVidualMoviePageState extends State<IndiVidualMoviePage> {
                         ],
                       ),
                     ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? MediaQuery.of(context).size.height * 0.4
+                  : MediaQuery.of(context).size.height * 0.70,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Padding(
+                    padding: MediaQuery.of(context).orientation ==
+                            Orientation.portrait
+                        ? EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.02,
+                          )
+                        : EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.01,
+                          ),
+                    child: BigTextWidget(
+                      fontSize: 22,
+                      text: "Select Date And Time",
+                      color: Colors.white,
+                      textOverflow: TextOverflow.visible,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  Container(
+                    margin: MediaQuery.of(context).orientation ==
+                            Orientation.portrait
+                        ? EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.02,
+                          )
+                        : EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.03,
+                          ),
+                    height: MediaQuery.of(context).orientation ==
+                            Orientation.portrait
+                        ? MediaQuery.of(context).size.height * 0.14
+                        : MediaQuery.of(context).size.height * 0.26,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: movieDates.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            indexChanger(index);
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            margin: MediaQuery.of(context).orientation ==
+                                    Orientation.portrait
+                                ? EdgeInsets.symmetric(
+                                    horizontal:
+                                        MediaQuery.of(context).size.width *
+                                            0.01,
+                                  )
+                                : EdgeInsets.symmetric(
+                                    horizontal:
+                                        MediaQuery.of(context).size.width *
+                                            0.01,
+                                  ),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(9),
+                              gradient: LinearGradient(
+                                colors: [
+                                  Colors.blueGrey.shade300,
+                                  Colors.blue.shade400,
+                                ],
+                              ),
+                            ),
+                            width: MediaQuery.of(context).orientation ==
+                                    Orientation.portrait
+                                ? MediaQuery.of(context).size.width * 0.2
+                                : MediaQuery.of(context).size.width * 0.12,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                BigTextWidget(
+                                  fontSize: 18,
+                                  text: movieDates[index]['Day'],
+                                  color: Colors.white,
+                                  textOverflow: TextOverflow.visible,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                                SizedBox(
+                                  height: MediaQuery.of(context).orientation ==
+                                          Orientation.portrait
+                                      ? MediaQuery.of(context).size.height *
+                                          0.01
+                                      : MediaQuery.of(context).size.height *
+                                          0.02,
+                                ),
+                                BigTextWidget(
+                                  fontSize: 20,
+                                  text: movieDates[index]['Date'],
+                                  color: Colors.white,
+                                  textOverflow: TextOverflow.visible,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: MediaQuery.of(context).orientation ==
+                            Orientation.portrait
+                        ? EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.02,
+                          )
+                        : EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * 0.02,
+                          ),
+                    height: MediaQuery.of(context).orientation ==
+                            Orientation.portrait
+                        ? MediaQuery.of(context).size.height * 0.04
+                        : MediaQuery.of(context).size.height * 0.09,
+                    decoration: const BoxDecoration(),
+                    child: ListView.builder(
+                      itemCount: movieDates.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(9),
+                            gradient: LinearGradient(
+                              colors: [
+                                Colors.blueGrey.shade300,
+                                Colors.blue.shade400,
+                              ],
+                            ),
+                          ),
+                          width: MediaQuery.of(context).orientation ==
+                                  Orientation.portrait
+                              ? MediaQuery.of(context).size.width * 0.22
+                              : MediaQuery.of(context).size.width * 0.12,
+                          margin: MediaQuery.of(context).orientation ==
+                                  Orientation.portrait
+                              ? EdgeInsets.symmetric(
+                                  horizontal:
+                                      MediaQuery.of(context).size.width * 0.01,
+                                )
+                              : EdgeInsets.symmetric(
+                                  horizontal:
+                                      MediaQuery.of(context).size.width * 0.01,
+                                ),
+                          child: BigTextWidget(
+                            fontSize: 18,
+                            text: movieDates[index]['Time'],
+                            color: Colors.white,
+                            textOverflow: TextOverflow.visible,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        );
+                      },
+                    ),
                   )
                 ],
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: MediaQuery.of(context).orientation ==
+                        Orientation.portrait
+                    ? EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.04,
+                      )
+                    : EdgeInsets.symmetric(
+                        horizontal: MediaQuery.of(context).size.width * 0.06,
+                        vertical: MediaQuery.of(context).size.height * 0.08,
+                      ),
+                height:
+                    MediaQuery.of(context).orientation == Orientation.portrait
+                        ? MediaQuery.of(context).size.height * 0.06
+                        : MediaQuery.of(context).size.height * 0.12,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.blue.shade300,
+                      Colors.deepPurple.shade400,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: BigTextWidget(
+                  fontSize: 22,
+                  text: "Book Now",
+                  color: Colors.white,
+                  textOverflow: TextOverflow.visible,
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
           ],
