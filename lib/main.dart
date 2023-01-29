dc4b28a681ac8d5d44aac8d2d306438645a0cff9
hitesh parmar <hiteshparmar16320@gmail.com>
2023-01-29 08:51:17 +0530

    home page changed

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
               theme: ThemeData(
                 primarySwatch: Colors.blue,
               ),
-              home: const IndiVidualMoviePage(),
+              home: const HomeScreen(),
             );
           }
           if (snapshot.hasError) {
